6ad68dd66798d90e86766b6dc83ad4efc36f6e5c
Антонина Нарваткина <antoninanarvatkina@MacBook-Pro-Antonina.local>
2022-10-09 14:40:50 +0300

    detect native speech

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        speech = request.form["speech"]
         response = openai.Completion.create(
-            model="text-davinci-002",
-            prompt=generate_prompt(animal),
+            model="text-curie-001",
+            prompt=generate_prompt(speech),
             temperature=0.6,
         )
         return redirect(url_for("index", result=response.choices[0].text))
@@ -22,14 +22,10 @@ def index():
     return render_template("index.html", result=result)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
+@app.route('/form-example')
+def form_example():
+	return 'Form Data Example'
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+
+def generate_prompt(speech):
+    return f"""Tell whether the speaker is native English or not. Speaker: {speech.capitalize()} Answer:"""
